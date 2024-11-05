029ff2fa60bcbecc9ae3913fed1b24871787d9c6
KhaNiwat <kchaos1150@gmail.com>
2024-11-05 19:48:06 +0700

    Test
    Test

--- cslogbook/backend/server.js
+++ cslogbook/backend/server.js
@@ -36,8 +36,8 @@ const students = [
     username: "jane_smith02",
     password: "12345678",
     studentID: "87654321",
-    firstName: "Jane",
-    lastName: "Smith"
+    firstName: "ja",
+    lastName: "Smth"
   }
 ];
 
