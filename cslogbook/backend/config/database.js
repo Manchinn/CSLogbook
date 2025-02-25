64d1a1250b7a9d15162a480bce099c00ba0ff82c
Manchinn <chinnakrit150@gmail.com>
2025-02-25 21:34:12 +0700

    feat: เปลี่ยนที่อยู่โฮสต์และชื่อฐานข้อมูลในการตั้งค่าการเชื่อมต่อ MySQL เปลี่ยนการเชื่อมต่อdatabase ไปยัง google cloud server

--- cslogbook/backend/config/database.js
+++ cslogbook/backend/config/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || '127.0.0.1',
+  host: process.env.DB_HOST || '34.72.255.66',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || 'root',
-  database: process.env.DB_NAME || 'cs_logbook',
+  database: process.env.DB_NAME || 'cslogbook',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
